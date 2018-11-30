3a0ef83e9058d5ad8397185a738c66db6d8a91dd
Caleb <caleb.coates@gmail.com>
2018-11-30 15:48:56 -0500

    Add files via upload

--- static bench locator.py
+++ static bench locator.py
@@ -1,18 +1,127 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 from datetime import *
+import os
+import webbrowser
 
-
+mainpath = ("/")
+filename = ("")
+finfile = ("")
+fullpath = ("")
+simpname = ("")
+sheetlist = []
 stations = dict()
+itemlist = []
+sheetfound = []
 
-def xmltofilelist():
-    print("blah")
+def xmltofilelist(basepath,file):
+    print("base path " + basepath)
+    print("file " + file)
+    xmlfile = str(basepath + "/" + file + "_files/filelist.xml")
+    print(xmlfile)
+    x = []
+    with open (xmlfile, "r+") as f:
+        for line in f:
+            if 'sheet' in line and '.htm' in line:
+                x.append(line[15:27])
+        for items in x:
+            print("Found:" + items)
+            if  len(x) == 0:
+                print("No map files found")
+    return(x)
+        
 
 def repair():
-    print("repair")
+    global mainpath
+    global filename
+    global simpname
+    global sheetlist
+    global finfile
+    global sheetfound
+    tabpathfull = (mainpath + "/" + simpname + "_files/tabstrip.htm")
+    tabpathnew = (mainpath + "/" + simpname + "_files/tabstripdump.htm")
+    fullfile = (mainpath + "/" + filename)
+    print(fullfile)
+    finfile = fullfile.replace(simpname, simpname + "dump")
+    print(finfile)
+    #tabstrip repair
+    print("Repairing Tabstrip: ")
+    with open (tabpathfull, "r+") as f, open(tabpathnew,"w+") as p:
+        for line in f:
+            if simpname in line:
+                print("before: " + line)
+                line = line.replace(simpname + ".htm", simpname + "dump.htm")
+                p.write(line)
+            elif any(x in line for x in sheetfound):
+                line = line.replace(".htm", "dump.htm")
+                line = line.replace("#FFFFFF","#00f1be")
+                p.write(line)
+            elif any(x in line for x in sheetlist):
+                line = line.replace(".htm", "dump.htm")
+                p.write(line)
+            else:
+                p.write(line)
+        p.close()
+        
+    with open (fullfile, "r+") as w, open(finfile,"w+") as r:
+        for line in w:
+            if filename in line:
+                print("before: " + line)
+                filenamenew = filename.replace(".htm","dump.htm")
+                line = line.replace(filename, filenamenew)
+                r.write(line)
+                print("after: " + line)
+            elif "tabstrip.htm" in line:
+                line = line.replace("tabstrip.htm","tabstripdump.htm")
+                r.write(line)
+            elif any(x in line for x in sheetlist):
+                for x in sheetlist:
+                    if x in line:
+                      line = line.replace(".htm", "dump.htm")
+                      r.write(line)
+            else:
+                  r.write(line)
 
-def runit():
-    print(runit)
+        r.close()
+            
+
+
+def highlight():
+    global mainpath
+    global filename
+    global fullpath
+    global simpname
+    global stations
+    global itemlist
+    global sheetfound
+    global sheetlist
+    sheetfound = []
+    print(itemlist)
+    for items in sheetlist:
+        sheetpathfull = (mainpath + "/" + simpname + "_files/" + items)
+        oldsheet = items
+        items = items.replace(".htm","dump.htm")
+        sheetpathnew = (mainpath + "/" + simpname + "_files/" + items)
+        print("Searching " +sheetpathfull+ " for stations, writing stations to " + sheetpathnew + ".")
+        with open(sheetpathfull, "r+") as f, open(sheetpathnew, "w+") as p:
+            for line in f:
+                if any(x in line for x in itemlist):
+                    for s in itemlist:
+                        if s in line:
+                            item = s
+                    if oldsheet not in sheetfound:
+                        sheetfound.append(oldsheet)
+                    print("original line: " + line)
+                    newitem = ("""<span style="background:#008822">""" + item + """</span>""")
+                    line = line.replace(item + "<" , newitem + "<")
+                    p.write(line)
+                    print("replaced: " + line)
+                else:
+                    p.write(line)
+            p.close()
+            print("Items found on the following sheets: " + str(sheetfound))
+
+
 
 class Static:
     def __init__(self, interval, date, location, description, status, days):
@@ -53,8 +162,6 @@ class Statics:
         scrollbar.config(command=self.listy.yview)
         self.listy.pack(side=LEFT, fill=Y)
         scrollbar.pack(side=LEFT, fill=Y)
-        #scrollbar.place(x=140,y=150)
-        #self.listy.place(x=10, y=80)
 
         self.add_button = Button(master, text="Add Asset", command=self.add)
         self.add_button.place(x=280, y=45, in_=root)
@@ -157,18 +264,37 @@ class Statics:
 
         
     def generate(self):
-        print("generate map")
+        if self.listy.size() == 0:
+            print("No items in list to locate. Please add items")
+        else:
+            global itemlist
+            itemlist =  self.listy.get(0,END)
+            print("generate map")
+            highlight()
+            repair()
+            print(finfile)
+            webbrowser.open_new(finfile)
 
-    def map(self):
-        print("mapfilechosen")
 
     def select(self):
+        global filename
+        global finfile
+        global mainpath
+        global fullpath
+        global simpname
+        global sheetlist
         print("Select File.")
         name = askopenfilename(initialdir="C:/Users/Batman/Documents/Programming/tkinter/",
-                filetypes =(("Text File", "*.txt"),("All Files","*.*")),
-                title = "Choose a file."
+                filetypes =(("Web Page", "*.htm"),("All Files","*.*")),
+                title = "Choose map file."
                 )
-        print (name)
+        filename = os.path.basename(name)
+        mainpath = os.path.dirname(name)
+        s = filename.split('.')
+        simpname = s[0]
+        sheetlist = xmltofilelist(mainpath,simpname)
+        
+        
 
 
         
